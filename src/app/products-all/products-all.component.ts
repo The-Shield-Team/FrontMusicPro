4d2353da715820457237e572d13323609c183782
Marian Moreno <marianjmo06@gmail.com>
2023-06-17 09:57:06 -0400

    Card

--- src/app/products-all/products-all.component.ts
+++ src/app/products-all/products-all.component.ts
@@ -11,11 +11,21 @@ export class ProductsAllComponent implements OnInit {
   productType = this.route.snapshot.paramMap.get('productType');
   productCategory = this.route.snapshot.paramMap.get('productCategory');
   productSubcategory = this.route.snapshot.paramMap.get('productSubcategory');
+  cardCategories = [
+    {name: 'Instrumentos de Cuerdas', path: 'products/cuerdas' },
+    {name: 'Percusión', path: 'products/percusion'},
+    {name: 'Amplificadores', path: 'products/amplificadores'},
+    {name: 'Accesorios varios', path: 'products/accesorios'}
+  ];
 
   constructor(
     private route: ActivatedRoute,
     private productsService: ProductsService
   ) {}
+
+  redireccionar = (url: string) => {
+    location.href = url;
+  };
 
   products: any[] = [];
   ngOnInit() {
