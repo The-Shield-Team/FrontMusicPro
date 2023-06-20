5ce71f32a9e5969e2d326ab144c6b92594dcb30c
AlejandroFerrera <aferrerah1421@gmail.com>
2023-06-20 13:52:01 -0400

    Fix body height

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card'
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 @NgModule({
   declarations: [
@@ -65,7 +75,20 @@ import {MatButtonModule} from '@angular/material/button';
     NgbDropdownModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    HttpClientModule,
+    MatTableModule,
+    MatSortModule,
+    MatRadioModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  email = new FormControl('', [
+    Validators.required,
+    Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+  ]);
+  password = new FormControl('', [Validators.required]);
 
+  error: string = '';
+
+  getErrorEmailMessage() {
+    if (this.email.hasError('required')) {
+      return 'Debe introducir un correo';
+    }
+
+    return this.email.hasError('pattern') ? 'Correo no válido' : '';
+  }
+
+  getErrorPasswordMessage() {
+    if (this.password.hasError('required')) {
+      return 'Debe introducir una contraseña';
+    }
+    return '';
+  }
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  login() {
+    return 'login';
+  }
 }
