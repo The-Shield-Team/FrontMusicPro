816ac333f97499e757bb51ddda5b50bd13d472fd
AlejandroFerrera <aferrerah1421@gmail.com>
2023-06-21 21:24:32 -0400

    El push -f es con responsabilidad

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,23 +1,28 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../services/products.service';
 import { Router } from '@angular/router';
+import { CartItem } from '../interfaces/cart-item';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent {
-
   product: any;
   product_id: number = 0;
 
-  constructor(private productService: ProductsService, private route: Router) { }
+  constructor(
+    private productService: ProductsService,
+    private route: Router,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    
+
     //Obtener el id del producto desde la url
     this.product_id = parseInt(this.route.url.split('/')[2]);
 
@@ -31,6 +36,25 @@ export class ProductDetailComponent {
   }
 
   formatPrice(price: number) {
-    return new Intl.NumberFormat('cl-CL', { style: 'currency', currency: 'CLP' }).format(price);
+    return new Intl.NumberFormat('cl-CL', {
+      style: 'currency',
+      currency: 'CLP',
+    }).format(price);
+  }
+
+  addItemToCart() {
+    const cartItem: CartItem = {
+      id: this.product.id,
+      name: this.product.title,
+      price: this.product.price,
+      image: this.product.image,
+      quantity: 1,
+    };
+
+    const isInCart = this.cartService.isInCart(cartItem.id);
+
+    isInCart
+      ? this.cartService.changeQty(1, cartItem.id)
+      : this.cartService.addToCart(cartItem);
   }
 }
